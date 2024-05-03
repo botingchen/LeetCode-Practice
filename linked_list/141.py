cd77e1a36a1b2a11f3218008fdb8f0c08c79d9a6
botingchen <Brad8910@gmail.com>
2024-05-04 00:34:45 +0800

    blind75

---
+++ linked_list/141.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        visited_node = set()
+        cur_node = head
+        while cur_node:
+            if cur_node in visited_node:
+                return True
+            visited_node.add(cur_node)
+            cur_node = cur_node.next
+        return False
+        
+class Solution2(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
