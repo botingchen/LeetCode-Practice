a5fd95d4766c7f5f47881f5b52a799a6ec0d1fc7
botingchen <Brad8910@gmail.com>
2024-06-23 23:07:07 +0800

    add priority queue solution

--- cpp/array/347.cpp
+++ cpp/array/347.cpp
@@ -1,3 +1,36 @@
+//Priority Queue
+class Solution {
+public:
+    struct freq_order {
+        bool operator()(const pair<int, int> l, const pair<int, int> r){
+            if(l.second == r.second){
+                return l.first > r.first;
+            }
+            return l.second > r.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mymap; //first=num, second=freq
+        vector<int> ans;
+        for(auto i : nums){
+            mymap[i]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, freq_order> MinHeap;
+        for(auto&[key, val] : mymap){
+            MinHeap.push({key, val});
+            if(MinHeap.size() > k){
+                MinHeap.pop();
+            }
+        }
+        while(!MinHeap.empty()){
+            ans.push_back(MinHeap.top().first);
+            MinHeap.pop();
+        }
+        return ans;
+    }
+};
+
+//Bucket Sort
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
