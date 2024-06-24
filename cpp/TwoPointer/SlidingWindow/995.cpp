2818354a7baaebd92c482621f0db9917957a2fca
botingchen <Brad8910@gmail.com>
2024-06-25 02:07:56 +0800

    daily

---
+++ cpp/TwoPointer/SlidingWindow/995.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size();
+        int flipped = 0;
+        int res = 0;
+        vector<int> isFlipped(n, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (i >= k) {
+                // if(isFlipped[i - k] = 1 && flipped == 1){
+                //     flipped = 0;
+                // }
+                flipped ^= isFlipped[i - k];
+            }
+            if (flipped == nums[i]) {
+                if (i + k > n) {
+                    return -1;
+                }
+                isFlipped[i] = 1;
+                if(flipped == 0) flipped++;
+                else flipped--;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
